dbba66d3fd967ae4d09a82568062753a82383e1b
Takuya Suemura <takuyasuemura@gmail.com>
2023-10-04 09:03:29 +0900

    Show youtube video when adding a fitness record

--- App.js
+++ App.js
@@ -5,9 +5,14 @@ import {
   View,
   TextInput,
   TouchableOpacity,
+  SafeAreaView,
 } from "react-native";
 import { Icon } from "react-native-elements";
+import { WebView } from "react-native-webview";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+import { YOUTUBE_API_KEY } from "@env";
+
 
 const FitnessLogList = ({ logs }) => {
   return (
@@ -29,6 +34,7 @@ export default function App() {
   const [fitnessName, setFitnessName] = useState("");
   const [fitnessMinutes, setFitnessMinutes] = useState("");
   const [fitnessLogs, setFitnessLogs] = useState([]);
+  const [videoUrl, setVideoUrl] = useState("");
 
 
   useEffect(() => {
@@ -49,39 +55,71 @@ export default function App() {
     };
     const updatedFitnessLogs = [...fitnessLogs, newFitnessLog];
     setFitnessLogs(updatedFitnessLogs);
-    await AsyncStorage.setItem('fitnessLogs', JSON.stringify(updatedFitnessLogs));
-    console.log(`Added ${fitnessMinutes} minutes of ${fitnessName} to your fitness record!`);
-    setFitnessName('');
-    setFitnessMinutes('');
+    await AsyncStorage.setItem(
+      "fitnessLogs",
+      JSON.stringify(updatedFitnessLogs)
+    );
+    console.log(
+      `Added ${fitnessMinutes} minutes of ${fitnessName} to your fitness record!`
+    );
+    setFitnessName("");
+    setFitnessMinutes("");
+
+    // Search for a random YouTube video related to the fitness activity
+    const response = await axios.get(
+      "https://www.googleapis.com/youtube/v3/search",
+      {
+        params: {
+          q: `cute kittens shorts`,
+          part: "snippet",
+          type: "video",
+          videoDefinition: "high",
+          videoEmbeddable: true,
+          maxResults: 50,
+          key: YOUTUBE_API_KEY,
+        },
+      }
+    );
+    const videos = response.data.items;
+    const randomVideo = videos[Math.floor(Math.random() * videos.length)];
+
+    // Embed the video in your app using the WebView component
+    setVideoUrl(`https://www.youtube.com/embed/${randomVideo.id.videoId}`);
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Fitness Recorder</Text>
-      <View style={styles.inputContainer}>
-        <Icon name="dumbbell" type="font-awesome-5" style={styles.icon} />
-        <TextInput
-          style={styles.input}
-          placeholder="Fitness Name"
-          value={fitnessName}
-          onChangeText={setFitnessName}
-        />
-      </View>
-      <View style={styles.inputContainer}>
-        <Icon name="clock" type="font-awesome-5" style={styles.icon} />
-        <TextInput
-          style={styles.input}
-          placeholder="Minutes"
-          keyboardType="numeric"
-          value={fitnessMinutes}
-          onChangeText={setFitnessMinutes}
-        />
-      </View>
-      <TouchableOpacity style={styles.button} onPress={handleAddFitness}>
-        <Text style={styles.buttonText}>Add Fitness</Text>
-      </TouchableOpacity>
-      <FitnessLogList logs={fitnessLogs} />
-    </View>
+    <>
+      <WebView
+        source={{ uri: videoUrl }}
+      />
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.title}>Fitness Recorder</Text>
+        <View style={styles.inputContainer}>
+          <Icon name="dumbbell" type="font-awesome-5" style={styles.icon} />
+          <TextInput
+            style={styles.input}
+            placeholder="Fitness Name"
+            value={fitnessName}
+            onChangeText={setFitnessName}
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <Icon name="clock" type="font-awesome-5" style={styles.icon} />
+          <TextInput
+            style={styles.input}
+            placeholder="Minutes"
+            keyboardType="numeric"
+            value={fitnessMinutes}
+            onChangeText={setFitnessMinutes}
+          />
+        </View>
+        <TouchableOpacity style={styles.button} onPress={handleAddFitness}>
+          <Text style={styles.buttonText}>Add Fitness</Text>
+        </TouchableOpacity>
+
+        <FitnessLogList logs={fitnessLogs.slice(-5)} />
+      </SafeAreaView>
+    </>
   );
 }
 
